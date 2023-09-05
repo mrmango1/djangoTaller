1c4fb9ffaad9a20d4389795793b72639867a7a21
Anderson Grefa <andersongrefa@gmail.com>
2023-09-04 22:11:46 -0500

    feat: agregando modelos

--- product/admin.py
+++ product/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from .models import Category, ProductImage, Color, Size, Clothes
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Category, CategoryAdmin)
+
+class ProductImageAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(ProductImage, ProductImageAdmin)
+
+class ColorAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Color, ColorAdmin)
+
+class SizeAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Size, SizeAdmin)
+
+class ClothesAdmin(admin.ModelAdmin):
+    readonly_fields = ('created_at', 'updated_at')
+
+admin.site.register(Clothes, ClothesAdmin)
--- product/models.py
+++ product/models.py
@@ -6,6 +6,7 @@ from django.contrib.auth.models import User
 class Category(models.Model):
     name = models.CharField(max_length=200, verbose_name="Categoría")
     description = models.TextField(verbose_name="Descripción")
+    img = models.ImageField(upload_to="categories", null=True, blank=True, verbose_name="Imagen")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
 
@@ -15,9 +16,9 @@ class Category(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self):
-        return self.category
+        return self.name
 
-class ProductImages(models.Model):
+class ProductImage(models.Model):
     image = models.ImageField(upload_to="products", null=True, blank=True, verbose_name="Imagen Secundaria")
     description = models.CharField(max_length=200, verbose_name="Descripción")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
@@ -28,7 +29,7 @@ class ProductImages(models.Model):
         verbose_name_plural = "imágenes"
         ordering = ["-created_at"]
 
-class Colors(models.Model):
+class Color(models.Model):
     name = models.CharField(max_length=200, verbose_name="Color")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
@@ -39,7 +40,7 @@ class Colors(models.Model):
         ordering = ["-created_at"]
 
     def __str__(self):
-        return self.color
+        return self.name
 
 class Size(models.Model):
     size = models.CharField(max_length=200, verbose_name="Talla")
@@ -53,17 +54,32 @@ class Size(models.Model):
 
     def __str__(self):
         return self.size
+    
+class Brand(models.Model):
+    name = models.CharField(max_length=200, verbose_name="Marca")
+    img = models.ImageField(upload_to="brands", null=True, blank=True, verbose_name="Imagen")
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
+    updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
+
+    class Meta:
+        verbose_name = "marca"
+        verbose_name_plural = "marcas"
+        ordering = ["-created_at"]
+
+    def __str__(self):
+        return self.name
 
 class Product(models.Model):
     name = models.CharField(max_length=200, verbose_name="Nombre")
     description = models.TextField(verbose_name="Descripción")
     spects = models.TextField(verbose_name="Especificaciones")
     brand = models.CharField(max_length=200, verbose_name="Marca")
+    category = models.ManyToManyField("Category", verbose_name="Categorías")
     price = models.FloatField(verbose_name="Precio")
     stock = models.IntegerField(verbose_name="Stock")
     rating = models.FloatField(verbose_name="Calificación")
     mainImage = models.ImageField(upload_to="products", null=True, blank=True, verbose_name="Imagen Principal")
-    images = models.ManyToManyField(ProductImages, verbose_name="Imágenes")
+    images = models.ManyToManyField("ProductImage", verbose_name="Imágenes")
     created_by = models.ForeignKey(User, verbose_name="Creado por", on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creación")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Fecha de edición")
@@ -77,9 +93,8 @@ class Product(models.Model):
         return self.name
 
 class Clothes(Product):
-    size = models.ManyToManyField(Size, verbose_name="Tallas")
-    color = models.ManyToManyField(Colors, verbose_name="Colores")
-    categories = models.ManyToManyField(Category, verbose_name="Categorías")
+    size = models.ManyToManyField("Size", verbose_name="Tallas")
+    color = models.ManyToManyField("Color", verbose_name="Colores")
 
     class Meta:
         verbose_name = "ropa"
