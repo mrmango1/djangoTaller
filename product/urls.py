3ff7c85a4d2246a9cedf2905b9500963879906de
Anderson Grefa <andersongrefa@gmail.com>
2023-09-06 16:31:35 -0500

    feat: agregando vistas para filtrar productos

--- product/urls.py
+++ product/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path("", views.shop, name="shop"),
+    path("brand/<str:brand>", views.shopByBrand, name="shopByBrand"),
+    path("category/<str:category>", views.shopByCategory, name="shopByCategory"),
+    path("gender/<str:gender>", views.shopByGender, name="shopByGender"),
     path("<int:id_product>", views.productDetail, name="productDetail"),
 ]
--- product/views.py
+++ product/views.py
@@ -1,9 +1,45 @@
-from django.shortcuts import render
+import math
+from django.shortcuts import render, get_object_or_404
+from .models import Clothes
+
+def roundRating(rating):
+    return math.ceil(rating) if rating % 1 >= 0.5 else math.floor(rating)
 
 # Create your views here.
 
 def shop(request):
-    return render(request, "shop.html")
+    clothes = Clothes.objects.all()
+    for cloth in clothes:
+        cloth.rating_rounded = roundRating(cloth.rating)
+    return render(request, "shop.html", {'clothes': clothes})
+
+def shopByBrand(request, brand):
+    clothes = Clothes.objects.filter(brand=brand)
+    for cloth in clothes:
+        cloth.rating_rounded = roundRating(cloth.rating)
+    return render(request, "shop.html", {'clothes': clothes})
+
+def shopByCategory(request, category):
+    clothes = Clothes.objects.filter(category=category)
+    for cloth in clothes:
+        cloth.rating_rounded = roundRating(cloth.rating)
+    return render(request, "shop.html", {'clothes': clothes})
+
+def shopByGender(request, gender):
+    clothes = Clothes.objects.filter(gender=gender)
+    for cloth in clothes:
+        cloth.rating_rounded = roundRating(cloth.rating)
+    return render(request, "shop.html", {'clothes': clothes})
 
 def productDetail(request, id_product):
-    return render(request, "productDetail.html")
+    cloth = get_object_or_404(Clothes, id=id_product)
+    cloth.rating_rounded = roundRating(cloth.rating)
+    related_clothes = Clothes.objects.filter(brand=cloth.brand).exclude(id=cloth.id)
+    images = cloth.images.all()
+    list_images = []
+    for i in range(0, len(images), 3):
+        list_images.append(images[i:i+4])
+    print(list_images)
+    for related_cloth in related_clothes:
+        related_cloth.rating_rounded = roundRating(related_cloth.rating)
+    return render(request, "productDetail.html", {'clothDetail': cloth, 'relatedClothes': related_clothes, 'images': list_images})
